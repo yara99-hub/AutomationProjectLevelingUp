265884bfd89dffa96c9259be4c0fe69a6af5cdf3
yara99-hub <yara.mohsen81@gmail.com>
2025-08-29 20:31:19 +0300

    Second Task

---
+++ src/test/java/Tasks/SecondTaskTest.java
@@ -0,0 +1,35 @@
+package Tasks;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class SecondTaskTest {
+
+    private WebDriver driver;
+
+    /**
+     * Open Google Chrome
+     * Navigate to [<a href="https://duckduckgo.com/">...</a>]
+     * Assert that the DuckDuckGo logo is displayed
+     * Close Google Chrome
+     */
+
+    @Test
+    public void test(){
+        driver.get("https://duckduckgo.com/");
+        By logo = By.xpath("//div[contains(@class,'Desktop')]//img");
+        boolean isLogoDisplayed = driver.findElement(logo).isDisplayed();
+        Assert.assertTrue(isLogoDisplayed, "Logo is not displayed");
+    }
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+    }
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+}
